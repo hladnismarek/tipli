d3cf51c578fba8f0df59ef3b626959662e8a838e
hladnismarek <105645640+hladnismarek@users.noreply.github.com>
2024-05-15 23:28:27 +0200

    Delete vendor directory

--- vendor/contributte/middlewares/src/Exception/InvalidStateException.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types = 1);
-
-namespace Contributte\Middlewares\Exception;
-
-class InvalidStateException extends MiddlewareException
-{
-
-}
--- vendor/contributte/psr7-http-message/src/Exception/RuntimeException.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types = 1);
-
-namespace Contributte\Psr7\Exception;
-
-abstract class RuntimeException extends \RuntimeException
-{
-
-}
